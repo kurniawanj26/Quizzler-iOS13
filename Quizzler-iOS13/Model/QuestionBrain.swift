6d6c6470d3614185db63cbd1321a22eee59aa850
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2026-03-30 22:54:24 +0700

    struct, 2D array, progress view

---
+++ Quizzler-iOS13/Model/QuestionBrain.swift
@@ -0,0 +1,21 @@
+//
+//  QuestionBrain.swift
+//  Quizzler-iOS13
+//
+//  Created by Jayadi Kurniawan on 30/03/26.
+//  Copyright © 2026 The App Brewery. All rights reserved.
+//
+
+// This file is to handle all the logic
+
+struct QuestionBrain {
+    let questions = [
+        Question(q: "1 - 1 = 0", a: "True"),
+        Question(q: "1 + 1 = 4", a: "False"),
+        Question(q: "2 + 1 = 3", a: "True"),
+        Question(q: "1 + 1 = 2", a: "True"),
+        Question(q: "1 + 5 = 9", a: "False"),
+    ]
+    
+    
+}
